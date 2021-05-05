ec849f69c1595a53f1124352efd87b8900dd25fe
Abidur Rahman Akash <abidurrahman471@gmail.com>
2021-05-05 14:04:04 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+[Media Provider App](https://media-service-provider.web.app/).
+
+## Available Scripts
+
+It's a clone app of Spotify. Spotify is a digital music, podcast, and video service that gives you access to millions of songs and other content from creators all over the world.
+
+Technology Used:
+
+### React
+
+### Context API
+
+### Material UI
+
+### Spotify API
+
+### Spotify Player
---
+++ src/spotify.js
@@ -0,0 +1,27 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const clientId = "7a1af1a71371435aad2acf2d6e6b2bb2";
+const redirectUri = "https://media-service-provider.web.app/";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
